2beec9ff53a669b1c3506c554cee6a071191fd1d
Valey88 <146131996+Valey88@users.noreply.github.com>
2024-01-13 12:04:17 +0500

    Delete public_html directory

--- public_html/assets/include/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <div class="container">
-        <div  class="header_block">
-            <a href="home.php">Главная</a>
-            <a href="reminders.php">Напоминания</a>
-        </div>
-    </div>
-</header>
